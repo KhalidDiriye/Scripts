85ebe8230bbbf2291c6fc11c5f13879cf729854b
Khalid Y Diriye <kdiriye18@gmail.com>
2018-02-13 13:28:35 -0600

    wording

--- awk.sh
+++ awk.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # Khalid Diriye
-# Search through network logs using awk and match IP and timestamps
+# Search through network logs using awk and find IP and timestamps
 
 grep -F $2 | awk '{match($0,/inside:[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+/); ip = substr($0,RSTART,RLENGTH) ; match($1,/[0-9][0-9]\:[0-9][0-9]\:[0-9][0-9]/); timestamp = substr($1,RSTART,RLENGTH) ; print timestamp "," substr(ip,8)}' 
